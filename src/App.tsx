832626ce85a872ed2a3281c01b70b14f01502ac8
samuelwsz <samuelxwsz@gmail.com>
2023-11-17 19:43:56 -0300

    att

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import axios from "axios"
 import "./global.css"
-import { useAPI } from "./hooks/useAPI"
+//import { useAPI } from "./hooks/useAPI"
+
+import { useQuery } from "react-query"
 
 interface RepositoryProps {
   full_name: string
@@ -7,13 +10,38 @@ interface RepositoryProps {
 }
 
 function App() {
-  const { data: repositories, isFetching } = useAPI<RepositoryProps[]>(
+  /* const { data: repositories, isFetching } = useAPI<RepositoryProps[]>(
     "/users/samuelwsz/repos"
+  )*/
+
+  const { data, isFetching } = useQuery<RepositoryProps[]>(
+    "repos",
+    async () => {
+      const response = await axios.get(
+        "https://api.github.com/users/samuelwsz/repos"
+      )
+
+      return response.data
+    }
   )
 
   return (
     <>
-      <div>
+      <div className="flex justify-center">
+        <ul>
+          {isFetching && <p>Carregando...</p>}
+          {data?.map((repo) => {
+            return (
+              <li key={repo.full_name}>
+                <strong>{repo.full_name}</strong>
+                <p>{repo.description}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {/*
+     <div>
         <ul>
           {isFetching && <p>Carregando...</p>}
           {repositories?.map((repo) => {
@@ -26,6 +54,7 @@ function App() {
           })}
         </ul>
       </div>
+    */}
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { QueryClientProvider } from "react-query"
+import App from "./App.tsx"
+import { queryClient } from "./services/queryClient.ts"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
 )
